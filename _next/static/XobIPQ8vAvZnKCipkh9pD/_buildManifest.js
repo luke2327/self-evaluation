27948b553f4d594f845eee76de7984bed4a44075
luke2327 <qwepoi8526@gmail.com>
2021-12-03 18:49:48 +0900

    Deploy Next.js to gh-pages

---
+++ _next/static/XobIPQ8vAvZnKCipkh9pD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/efd83b4108bd113b.css","static/chunks/pages/index-7559517339d4594a.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
